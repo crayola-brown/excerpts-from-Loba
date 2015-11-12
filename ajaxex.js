4f2b0a35c4410b742e3cd4f77dede9640b0f98cc
Chelsea Parkkila <cmparkkila@gmail.com>
2015-11-12 14:54:10 -0800

    first commit

---
+++ ajaxex.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$.backstretch([
+		"loba-howl.jpg",
+		"beautiful-coyote.jpg",
+		"timber-wolf-wallpaper.jpg",
+		"mystic-wolf.jpg"],
+		{duration: 8000,fade: "slow"});
+});
